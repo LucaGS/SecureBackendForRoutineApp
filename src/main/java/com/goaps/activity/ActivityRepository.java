2a0e89d0374c058c5ff0a0369f859ece56bed1da
Luca Stieme <123728529+LucaGS@users.noreply.github.com>
2025-05-30 14:31:57 +0200

    Repofix

--- src/main/java/com/goaps/activity/ActivityRepository.java
+++ src/main/java/com/goaps/activity/ActivityRepository.java
@@ -1,6 +1,7 @@
 package com.goaps.activity;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ActivityRepository extends JpaRepository<Integer, Activity> {
+public interface ActivityRepository extends JpaRepository<Activity, Integer> {
 }
