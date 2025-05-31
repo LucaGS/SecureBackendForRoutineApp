8c46df16ab5f0d42cd85635e8eb894380a8d286e
Luca Stieme <123728529+LucaGS@users.noreply.github.com>
2025-05-31 17:52:10 +0200

    Update AcitvityController.java

--- src/main/java/com/goaps/activity/AcitvityController.java
+++ src/main/java/com/goaps/activity/AcitvityController.java
@@ -1,14 +1,26 @@
 package com.goaps.activity;
 
+import com.goaps.routine.RoutineRequest;
+import com.goaps.routine.RoutineResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("api/v1/Activity")
 public class AcitvityController {
     private final ActivityService activityService;
-
+    @PostMapping()
+    public ResponseEntity<ActivityResponse> Create(
+            @RequestBody ActivityRequest request
+    ){
+        return ResponseEntity.ok(activityService.create(request));
+    }
+    @GetMapping()
+    public List<ActivityResponse> listAllUserRoutines(@RequestParam int routineId) {
+        return activityService.getAllForRoutine(routineId);
+    }
 }
